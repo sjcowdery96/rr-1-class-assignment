6c272bda8c8a48fd5dc579a0e2f7931cea04f830
sjcowdery96 <sjc.cowdery@gmail.com>
2023-11-14 21:05:53 -0600

    added explaining comments

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
+//one line for imports
 import React, { useState } from 'react'
 
+//declare the main function
 function App() {
+  //react useState links this function to our counter variable 
+  // REACT SYNTAX --> const [state, setState] = useState(initialState)
   let [counter, changeCounter] = useState(1)
+  //makes that function global to the ENTIRE window so devs can use it in browser
   window.changeCounter = changeCounter
+  //returns our web-page
   return (
     <div>
       <h1>{counter}</h1>
       <h4>{counter}</h4>
+      <p>this page has been rendered {counter} times! Chill out!</p>
     </div>
   )
 }
-
+//export the process
 export default App;
